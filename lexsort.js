391052f14f047609e11d52cda3ba1611e4c09d51
Rich Middleditch <richmiddleditch@gmail.com>
2014-04-15 21:16:59 +0100

    Remove tabs

--- lexsort.js
+++ lexsort.js
@@ -1,41 +1,41 @@
 if (typeof define !== 'function') {
-	var define = require('amdefine')(module);
+    var define = require('amdefine')(module);
 }
 
 define(function(require) {
-	var removeArticles = function(string) {
-			if (!articleRegex) {
-				articleRegex = new RegExp(
-					'^(' + articles.join('|') + ') (.*)', 'gi'
-				);
-			}
-			return string.replace(articleRegex, '$2');
-		},
-		articles = ['a', 'an', 'the'],
-		articleRegex;
+    var removeArticles = function(string) {
+            if (!articleRegex) {
+                articleRegex = new RegExp(
+                    '^(' + articles.join('|') + ') (.*)', 'gi'
+                );
+            }
+            return string.replace(articleRegex, '$2');
+        },
+        articles = ['a', 'an', 'the'],
+        articleRegex;
 
-	return {
-		getArticles: function () {
-			return articles;
-		},
-		setArticles: function (values) {
-			articles = values;
-			articleRegex = null;
-		},
-		sort: function(strings) {
-			var result = strings.sort(function(a, b) {
-				var cleanA = removeArticles(a),
-					cleanB = removeArticles(b);
+    return {
+        getArticles: function () {
+            return articles;
+        },
+        setArticles: function (values) {
+            articles = values;
+            articleRegex = null;
+        },
+        sort: function(strings) {
+            var result = strings.sort(function(a, b) {
+                var cleanA = removeArticles(a),
+                    cleanB = removeArticles(b);
 
-				if (cleanA < cleanB) {
-					return -1;
-				} else if (cleanA === cleanB) {
-					return 0;
-				}
-				return 1;
-			});
+                if (cleanA < cleanB) {
+                    return -1;
+                } else if (cleanA === cleanB) {
+                    return 0;
+                }
+                return 1;
+            });
 
-			return result;
-		}
-	};
+            return result;
+        }
+    };
 });
--- tests/lexsort.spec.js
+++ tests/lexsort.spec.js
@@ -1,41 +1,41 @@
 var lexsort = require('../lexsort');
 
 describe('lexsort', function () {
-	it('can sort an array of strings', function () {
-		var result = lexsort.sort(['c', 'b', 'a']);
-		expect(result).toEqual(['a', 'b', 'c']);
-	});
+    it('can sort an array of strings', function () {
+        var result = lexsort.sort(['c', 'b', 'a']);
+        expect(result).toEqual(['a', 'b', 'c']);
+    });
 
-	it('can sort ignoring "the"', function () {
-		var result = lexsort.sort(['the zebra', 'the aardvark', 'monkey']);
-		expect(result).toEqual([
-			'the aardvark',
-			'monkey',
-			'the zebra'
-		]);
-	});
+    it('can sort ignoring "the"', function () {
+        var result = lexsort.sort(['the zebra', 'the aardvark', 'monkey']);
+        expect(result).toEqual([
+            'the aardvark',
+            'monkey',
+            'the zebra'
+        ]);
+    });
 
-	it('can ignore all articles in english', function () {
-		var result = lexsort.sort(['the zebra', 'an aardvark', 'a monkey']);
-		expect(result).toEqual([
-			'an aardvark',
-			'a monkey',
-			'the zebra'
-		]);
-	});
+    it('can ignore all articles in english', function () {
+        var result = lexsort.sort(['the zebra', 'an aardvark', 'a monkey']);
+        expect(result).toEqual([
+            'an aardvark',
+            'a monkey',
+            'the zebra'
+        ]);
+    });
 
-	it('can accept user defined articles to ignore', function () {
-		expect(lexsort.getArticles()).toEqual(['a', 'an', 'the']);
-		lexsort.setArticles(['a', 'an', 'the', 'y', 'yr', 'na']);
-		expect(lexsort.getArticles()).toEqual(['a', 'an', 'the', 'y', 'yr', 'na']);
-	});
+    it('can accept user defined articles to ignore', function () {
+        expect(lexsort.getArticles()).toEqual(['a', 'an', 'the']);
+        lexsort.setArticles(['a', 'an', 'the', 'y', 'yr', 'na']);
+        expect(lexsort.getArticles()).toEqual(['a', 'an', 'the', 'y', 'yr', 'na']);
+    });
 
-	it('uses user defined articles', function () {
-		var result = lexsort.sort(['y zebra', 'yr aardvark', 'na monkey']);
-		expect(result).toEqual([
-			'yr aardvark',
-			'na monkey',
-			'y zebra'
-		]);
-	})
+    it('uses user defined articles', function () {
+        var result = lexsort.sort(['y zebra', 'yr aardvark', 'na monkey']);
+        expect(result).toEqual([
+            'yr aardvark',
+            'na monkey',
+            'y zebra'
+        ]);
+    })
 });
