7092ba2178c4d74d09f992f9e3304fb569701233
Rich Middleditch <richmiddleditch@gmail.com>
2014-04-15 20:29:40 +0100

    Minor speed improvements

--- lexsort.js
+++ lexsort.js
@@ -4,12 +4,15 @@ if (typeof define !== 'function') {
 
 define(function(require) {
 	var removeArticles = function(string) {
-			var regex = new RegExp(
-				'^(' + articles.join('|') + ') (.*)', 'gi'
-			);
-			return string.replace(regex, '$2');
+			if (!articleRegex) {
+				articleRegex = new RegExp(
+					'^(' + articles.join('|') + ') (.*)', 'gi'
+				);
+			}
+			return string.replace(articleRegex, '$2');
 		},
-		articles = ['a', 'an', 'the'];
+		articles = ['a', 'an', 'the'],
+		articleRegex;
 
 	return {
 		getArticles: function () {
@@ -17,12 +20,16 @@ define(function(require) {
 		},
 		setArticles: function (values) {
 			articles = values;
+			articleRegex = null;
 		},
 		sort: function(strings, callback) {
 			var result = strings.sort(function(a, b) {
-				if (removeArticles(a) < removeArticles(b)) {
+				var cleanA = removeArticles(a),
+					cleanB = removeArticles(b);
+
+				if (cleanA < cleanB) {
 					return -1;
-				} else if(removeArticles(a) === removeArticles(b)) {
+				} else if (cleanA === cleanB) {
 					return 0;
 				}
 				return 1;
